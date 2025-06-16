236c0b2b0a2c201e3697e8e39f5ba754e9996959
Kai Ler <133531148+kailermai@users.noreply.github.com>
2025-06-16 18:23:41 +0800

    Initalised react app with vite

---
+++ src/services/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDo6PLH4OmQ58FFOqse3ArEypdF8u-QfM0",
+  authDomain: "bambistic-7be6f.firebaseapp.com",
+  projectId: "bambistic-7be6f",
+  storageBucket: "bambistic-7be6f.firebasestorage.app",
+  messagingSenderId: "657092262530",
+  appId: "1:657092262530:web:c29722e3cdf907eb4c529b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
